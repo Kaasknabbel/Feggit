2fe5adf08ccf8bd3585ea1b8d629c4e5132c7a3d
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-09-27 00:44:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const PREFIX = "!";
+
+var servers = {};
 
 client.on('ready', () => {
     console.log('I am a feggit!');
 });
 
 client.on('message', message => {
+    if (message.author.equals(client.user)) return;
+    
+    if (!message.content.startsWith(PREFIX)) return;
+    
     if (message.content === 'ping') {
       message.reply('pong');
     }
@@ -13,7 +20,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'Tell me a story') {
-      message.reply('Ronnie is a feggit');
+      message.channel.sendMessage('Ronnie is a feggit');
     }
 });
 
